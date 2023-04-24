6857adb30ca6b37d05cc431e5bf630d01c0b436c
papontemjetix <papontem@gmail.com>
2023-04-24 14:34:13 -0400

    Initialization

---
+++ scripts.js
@@ -0,0 +1,53 @@
+/**
+ * Part One The Vehicle Class
+ * 
+ */
+
+// let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+// myFirstVehicle.honk(); // "Beep."
+
+// let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+// myFirstVehicle.toString(); // "The vehicle is a Honda Monster Truck from 1999."
+
+/**
+ * Part Two The Car Class
+ * 
+ */
+
+// let myFirstCar = new Car("Toyota", "Corolla", 2005);
+// myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
+// myFirstCar.honk();     // "Beep."
+// myFirstCar.numWheels;  // 4
+
+/**
+ * Part Three The Motor Cycle Class
+ *
+ */
+
+// let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+// myFirstMotorcycle.toString();
+// // "The vehicle is a Honda Nighthawk from 2000."
+
+// myFirstMotorcycle.honk();     // "Beep."
+// myFirstMotorcycle.revEngine(); // "VROOM!!!"
+// myFirstMotorcycle.numWheels;  // 2
+
+/**
+ * Part Four The Garage Class
+ */
+
+// let garage = new Garage(2);
+// garage.vehicles; // []
+// garage.add(new Car("Hyundai", "Elantra", 2015)); // "Vehicle added!"
+// garage.vehicles; // [Car]
+// garage.add("Taco"); // "Only vehicles are allowed in here!"
+
+// garage.add(new Motorcycle("Honda", "Nighthawk", 2000));
+// // "Vehicle added!"
+// garage.vehicles; // [Car, Motorcycle]
+
+// garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
+// // "Sorry, we're full."
+
+
